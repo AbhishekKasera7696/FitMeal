2af8adcd3628224641bcd133e8ba992c81a2d44b
AbhishekKasera7696 <akasera62@gmail.com>
2022-03-01 21:07:17 +0530

    Here i have created Nav bar with import and export.
    Here i have created Nav bar with import and export.

---
+++ components/navbar.js
@@ -0,0 +1,18 @@
+function navbar(){
+    return `<div class="logo">
+    <img src="https://i.ibb.co/Kxg4KW0/logo-black.png">
+</div>
+
+<div class="menu">
+    <div class="home">Home</div>
+    <div class="home">Meal Plans</div>
+    <div class="home">Health Coaching</div>
+    <div class="home">Product</div>
+    <div class="home">Blog</div>
+    <div class="home">About Us</div>
+</div>
+<div><button class="btn">Call:9100347480</button></div>
+`
+}
+
+export default navbar;
---
+++ index.js
@@ -0,0 +1,7 @@
+import navbar from "./components/navbar.js";
+
+let header = document.getElementById("navbar");
+
+header.innerHTML = navbar();
+
+
